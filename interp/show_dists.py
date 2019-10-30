9d856134e5a97f8070a6b88626a083d02f32ad86
matthew.phillips <matthew.phillips@lifeomic.com>
2019-10-30 11:53:52 -0400

    Added visualization of pairwise distances

--- interp/show_dists.py
+++ interp/show_dists.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import sys
+from collections import OrderedDict
 from scipy.spatial.distance import euclidean
 
 sys.path.append( os.path.dirname(os.path.dirname(os.path.abspath(__file__))) )
@@ -19,17 +20,45 @@ pj = os.path.join
 HOME = os.path.expanduser("~")
 
 
+def plot_dists(prop_dict, num_bins):
+    N = len(prop_dict)
+    figsize = (10,8)
+    num_rows = int( np.floor( np.sqrt(N) ) )
+    num_cols = int( np.ceil(N/num_rows) )
+    fig,axs = plt.subplots(num_rows, num_cols, figsize=figsize)
+    prop_list = list( prop_dict.items() )
+    for ct in range(N):
+        i = ct // num_cols
+        j = ct % num_cols
+        ax = axs[i][j]
+        n = prop_list[ct][0]
+        dist = prop_list[ct][1]
+        bins = np.linspace(0, np.quantile(dist, 0.99), num_bins)
+        ax.set_title("n = %d" % n)
+        ax.hist(dist, bins=bins)
+
+    for i in range(num_rows):
+        for j in range(num_cols):
+            ax = axs[i][j]
+            ax.set_xticks([])
+            ax.set_yticks([])
+    plt.tight_layout(rect=[0, 0.0, 1, 0.95])
+
+
 def main(cfg):
     data = U.make_wave_data(cfg["num_waves"], cfg["num_points"], cfg["sigma"])
     nearest_dict = U.make_nearest_dict(cfg["num_starts"], cfg["n_range"], data)
-
-    entropies = []
+    prop_dict = OrderedDict()
     for n in range(1,cfg["n_range"]+1):
-        bin_sz = cfg["bin_size"]
-        dists = U.get_pairwise_dists(nearest_dict, data, n)
-        entropies.append( U.calc_entropy(dists, bin_sz) )
-    plt.plot(list(range(1,cfg["n_range"]+1)), entropies)
-    plt.title("Local entropy as function of N nearest neighbors")
+        if cfg["property"] == "pwise-dist":
+            dists = U.get_pairwise_dists(nearest_dict, data, n)
+        else:
+            raise NotImplementedError( cfg["property"] )
+        prop_dict[n] = dists        
+        
+    plot_dists(prop_dict, cfg["num_bins"])
+
+    plt.suptitle("Distribution '%s' as function of n" % cfg["property"])
     plt.show()
 
 
@@ -40,7 +69,11 @@ if __name__ == "__main__":
     parser.add_argument("--num-waves", type=int, default=4)
     parser.add_argument("--num-starts", type=int, default=10)
     parser.add_argument("--n-range", type=int, default=40)
-    parser.add_argument("--bin-size", type=float, default=0.1)
+    parser.add_argument("--num-bins", type=int, default=50)
+
+    parser.add_argument("--property", default="pwise-dist",
+            choices=["pwise-dist"])
+
     cfg = vars( parser.parse_args() )
     main(cfg)
 
