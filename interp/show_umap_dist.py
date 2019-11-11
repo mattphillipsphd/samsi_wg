ba06cd11eb3bed42346ee39df2899e803771b9a4
Matt Phillips <mattphillipsphd@gmail.com>
2019-11-10 20:02:53 -0500

    Fixed show_umap_dist bugs

--- interp/show_umap_dist.py
+++ interp/show_umap_dist.py
@@ -19,11 +19,11 @@ pj = os.path.join
 HOME = os.path.expanduser("~")
 
 def main(cfg):
-    data = U.make_wave_data(cfg["num_waves"], cfg["num_pts"], cfg["sigma"])
+    data = U.make_wave_data(cfg["num_waves"], cfg["num_points"], cfg["sigma"])
     data_name = "waves(4)"
     labels = []
     for c in range( cfg["num_waves"] ):
-        labels += [c] * cfg["num_pts"]
+        labels += [c] * cfg["num_points"]
 
     ns = list(range(2, cfg["n_range"]))
     dis = []
@@ -37,12 +37,20 @@ def main(cfg):
     plt.xlabel("# nearest neighbors")
     plt.ylabel("Dunn Index")
     plt.title("Dunn Index on %s" % data_name)
+    if cfg["show_plot"]:
+        plt.show()
+    if not pe(cfg["output_dir"]):
+        os.makedirs( cfg["output_dir"] )
+    plt.savefig( pj(cfg["output_dir"], "umap_dunn_index.png") )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--n-range", type=int, default=20)
     parser.add_argument("--min-dist", type=float, default=0.3)
     parser.add_argument("--umap-metric", type=str, default="correlation")
+    parser.add_argument("-o", "--output-dir", type=str,
+            default=pj(HOME, "Repos/mattphillipsphd/samsi_wg/interp/output"))
+    parser.add_argument("--show-plot", action="store_true")
 
     parser.add_argument("--sigma", type=float, default=0.025)
     parser.add_argument("--num-points", type=int, default=200)
