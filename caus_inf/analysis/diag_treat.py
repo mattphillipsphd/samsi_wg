3dec7c6bddcfc9b1af7b6bad5c4338a85366a128
matthew.phillips <matthew.phillips@lifeomic.com>
2020-07-31 10:34:52 -0400

    Conditional probbility matrices for treatment/diagnosis subcategories

--- caus_inf/analysis/diag_treat.py
+++ caus_inf/analysis/diag_treat.py
@@ -18,10 +18,76 @@ pj = os.path.join
 HOME = os.path.expanduser("~")
 
 
+g_cats1 = ['burns/trauma', 'cardiovascular', 'endocrine', 'gastrointestinal',
+        'general', 'hematology', 'infectious diseases', 'neurologic',
+        'oncology', 'pulmonary', 'renal', 'surgery', 'toxicology',
+        'transplant']
+
 def get_pt_ids(data_dir):
     d = pj(os.path.dirname(data_dir), "static_pts")
     pt_ids = [os.path.splitext(f)[0] for f in os.listdir(d)]
     return pt_ids
+
+def make_cond_probs(d1, t1, d_pids, t_pids, data_dir, title="global",
+        level=None):
+    unique_d1s = sorted( np.unique(d1) )
+    unique_t1s = sorted( np.unique(t1) )
+    print("Diagnoses:", unique_d1s)
+    print("Treatments:", unique_t1s)
+
+    d1_d = { diag : i for i,diag in enumerate(unique_d1s) }
+    t1_d = { treat : i for i,treat in enumerate(unique_t1s) }
+
+    diag_idxs = np.array( [d1_d[diag] for diag in d1] )
+    treat_idxs = np.array( [t1_d[treat] for treat in t1] )
+
+    M = len(unique_d1s)
+    N = len(unique_t1s)
+    joint_counts,total_counts = np.zeros((N,M)), np.zeros((N,M))
+
+    for d_pid in d_pids.unique():
+        dp_idxs = np.array(d_pids==d_pid)
+        tp_idxs = np.array(t_pids==d_pid)
+
+        diags_p = diag_idxs[dp_idxs]
+        treats_p = treat_idxs[tp_idxs]
+        #print(len(diags_p), len(treats_p))
+
+        for j in diags_p:
+            for i in range(N):
+                total_counts[i][j] += 1
+                if i in treats_p:
+                    joint_counts[i][j] += 1
+
+    joint_prob = joint_counts/total_counts
+    ax = plt.imshow(joint_prob, cmap="gray", interpolation="none", vmin=0.0,
+            vmax=1.0)
+    plt.gcf().colorbar(ax)
+    plt.yticks( list(range(N)), unique_t1s )
+    plt.xticks( list(range(M)), unique_d1s, rotation="vertical" )
+    plt.title(title)
+    plt.margins(1.0)
+    plt.subplots_adjust(bottom=0.5)
+    if level is None:
+        plt.savefig( pj(data_dir, f"treat_diag_{title}.png") )
+    else:
+        plt.savefig( pj(data_dir, f"treat_diag_{title}_{level:02d}.png") )
+    plt.close()
+
+
+def make_subprobs(diag_toks, treat_toks, d_pids, t_pids, data_dir):
+    d1 = np.array( [x[0] for x in diag_toks] )
+    t1 = np.array( [x[0] for x in treat_toks] )
+    d2 = np.array( [x[1] for x in diag_toks] )
+    t2 = np.array( [x[1] for x in treat_toks] )
+    for i,cat1 in enumerate(g_cats1):
+        d2_i = d2[d1 == cat1]
+        t2_i = t2[t1 == cat1]
+        d_pids_i = d_pids[d1 == cat1]
+        t_pids_i = t_pids[t1 == cat1]
+        cat1 = cat1.replace("/", "_")
+        make_cond_probs(d2_i, t2_i, d_pids_i, t_pids_i, data_dir, title=cat1,
+                level=i)
 
 def main(cfg):
     data_dir = os.path.abspath( cfg["data_dir"] )
@@ -46,50 +112,14 @@ def main(cfg):
     diag_toks = [x.split("|") for x in diags]
     treat_toks = [x.split("|") for x in treats]
     
-    d1 = [x[0] for x in diag_toks]
-    t1 = [x[0] for x in treat_toks]
-
-    unique_d1s = sorted( np.unique(d1) )
-    unique_t1s = sorted( np.unique(t1) )
-    print("Diagnoses")
-    print(unique_d1s)
-    print("Treatments")
-    print(unique_t1s)
-
-    d1_d = { diag : i for i,diag in enumerate(unique_d1s) }
-    t1_d = { treat : i for i,treat in enumerate(unique_t1s) }
-
-    diag_idxs = np.array( [d1_d[diag] for diag in d1] )
-    treat_idxs = np.array( [t1_d[treat] for treat in t1] )
-
     d_pids = diag_df.patientunitstayid
     t_pids = treat_df.patientunitstayid
     
-    M = len(unique_d1s)
-    N = len(unique_t1s)
-    joint_counts,total_counts = np.zeros((N,M)), np.zeros((N,M))
+    d1 = np.array( [x[0] for x in diag_toks] )
+    t1 = np.array( [x[0] for x in treat_toks] )
+    make_cond_probs(d1, t1, d_pids, t_pids, data_dir)
 
-    for d_pid in d_pids.unique():
-        dp_idxs = np.array(d_pids==d_pid)
-        tp_idxs = np.array(t_pids==d_pid)
-
-        diags_p = diag_idxs[dp_idxs]
-        treats_p = treat_idxs[tp_idxs]
-        #print(len(diags_p), len(treats_p))
-
-        for j in diags_p:
-            for i in range(N):
-                total_counts[i][j] += 1
-                if i in treats_p:
-                    joint_counts[i][j] += 1
-
-    joint_prob = joint_counts/total_counts
-    print(joint_prob)
-    ax = plt.imshow(joint_prob, cmap="gray", interpolation="none", vmin=0.0,
-            vmax=1.0)
-    plt.gcf().colorbar(ax)
-    plt.savefig( pj(data_dir, "treat_diag_joint_prob1.png") )
-    plt.close()
+    make_subprobs(diag_toks, treat_toks, d_pids, t_pids, data_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
