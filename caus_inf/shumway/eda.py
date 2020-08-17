6ff3f39b0f8098e20265e1fd0cf652aff7cd2c70
Matthew Phillips <matt@Matthews-MacBook-Pro.local>
2020-08-17 17:34:52 -0400

    WIP detrending eda code

--- caus_inf/shumway/eda.py
+++ caus_inf/shumway/eda.py
@@ -4,5 +4,18 @@ Scripts to do the EDA of chapter 2 in Shumway and Stoffer
 
 import os
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
+def detrend(Y):
+    """
+    Returns detrended version of Y
+    """
+    
+    X = np.array( range( len( Y) ) ).reshape(-1, 1)
+    reg = LinearRegression().fit(X, Y)
+    
+    trend = reg.predict(X)
+    detY = Y - trend
+
+    return detY
 
